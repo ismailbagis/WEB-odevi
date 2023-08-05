bfce463ec607a888abd87d647b75ca83f09b67c3
ismailbagis <137333448+ismailbagis@users.noreply.github.com>
2023-08-05 23:04:57 +0300

    api.js.de açıklama satırları eklendi.
    api.js.de açıklama satırları eklendi.

--- api.js
+++ api.js
@@ -6,16 +6,18 @@ async function showData() {
       url: "https://api.nasa.gov/planetary/apod?api_key=mJkN2FrHOPUiiTwlkTwFGVVSBcHuYhwC30gkPG1D",
     }
   );
-  var data = await response.json();
-const apibox =document.querySelector(".apibox");
+  var data = await response.json(); /*JSON formatı js formata aktarma */
+/*API sınıflarına erişim*/
+  const apibox =document.querySelector(".apibox");
 const apititle =document.querySelector(".api-title");
 const apidate =document.querySelector(".api-date");
 const apidescription =document.querySelector(".api-description");
 
 
 apibox.innerHTML=`
-<img src="${data.url}" alt="" />
+<img src="${data.url}" alt="Resim bulunamadı." />
 `
+/*API fonksiyonları*/
 apititle.innerHTML=`
 ${data.title}
 `
@@ -24,9 +26,8 @@ ${data.date}
 `
 apidescription.innerHTML=`
 ${data.explanation}
-
 `
-
+/*KONSOLA VERİ AKTARMA*/
  console.log(data);
 }
 
